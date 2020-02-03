702dbba61a4642b9d11f305ed6f6851aac2aed48
hieppv <hieppv@vng.com.vn>
2020-02-03 15:12:25 +0700

    Create mixins.scss by task grunt-concat

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,5 @@
+const sass = require('node-sass');
+
 module.exports = function (grunt) {
 
 	var banner = '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
@@ -6,7 +8,34 @@ module.exports = function (grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		pkg: require('./package'),
-        
+
+		concat: {
+			options: {
+				banner: banner
+			},
+			build: {
+				src: ['mixins/*.scss', '!mixins/_zsass-mixins.scss'],
+				dest: 'mixins.scss',
+			}
+		},
+
+		sass: {
+			css: {
+				options: {
+					implementation: sass,
+					unixNewlines: true,
+					style: 'expanded',
+					banner: banner,
+					sourceMap: true
+				},
+				dist: {
+					files: {
+						'test/css/main.css': 'test/scss/main.scss'
+					}
+				}
+			}
+		},
+
 		sassdoc: {
 			default: {
 				src: 'mixins.scss',
@@ -15,13 +44,24 @@ module.exports = function (grunt) {
 					basePath: 'https://github.com/chuoivh10/zsass-mixins',
 				},
 			},
-		},
+		}
 	});
 
+	grunt.event.on('sassdoc.start', function (target, src, dest) {
+		grunt.log.writeln(target + ': compiling ' + src + ' to ' + dest)
+	})
+
+	grunt.event.on('sassdoc.done', function (target, src, dest) {
+		grunt.log.writeln(target + ': ' + src + ' compiled to ' + dest)
+	})
+
 	// Load some stuff
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-sassdoc');
 
 	// Default task
-	grunt.registerTask('default', ['sassdoc']);
+	grunt.registerTask('default', ['concat', 'sass']);
+	grunt.registerTask('doc', ['concat', 'sassdoc']);
 
 };
